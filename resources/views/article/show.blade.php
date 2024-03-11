2c9134039cfd5f0c363ad7b400bf953db5984011
Sasan <sanei.mohamad313@gmail.com>
2024-03-11 16:36:57 +0330

    Add admin actions into dashboard show article and trashed-dashboard

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,13 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <x-nav-link href="{{route('articles.index')}}">
-            {{ __('My articles') }}
-        </x-nav-link>
+        @if(! auth()->user()->is_admin)
+            <x-nav-link href="{{route('articles.index')}}">
+                {{ __('My articles') }}
+            </x-nav-link>
+        @endif
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg relative rounded-xl overflow-auto">
+            <div
+                class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg relative rounded-xl overflow-auto">
                 <div class="p-6 text-gray-900 dark:text-gray-100 my-8">
                     @auth
                         <table class="border-collapse table-fixed w-full text-sm">
@@ -28,7 +31,7 @@
                                 <th class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                                     Publication status
                                 </th>
-                                @if($article->author()->is(auth()->user()))
+                                @if($article->author()->is(auth()->user()) || auth()->user()->is_admin)
                                     <th class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                                         -
                                     </th>
@@ -36,19 +39,42 @@
                             </tr>
                             </thead>
                             <tbody class="dark:bg-slate-800 ">
-                                <tr>
-                                    <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->title}}</td>
-                                    <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->content}}</td>
-                                    <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->author->name}}</td>
-                                    <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->publication_date->toDateString()}}</td>
-                                    <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->publication_status->name}}</td>
+                            <tr>
+                                <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->title}}</td>
+                                <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->content}}</td>
+                                <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->author->name}}</td>
+                                <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->publication_date->toDateString()}}</td>
+                                <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{$article->publication_status->name}}</td>
+                                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
                                     @if($article->author()->is(auth()->user()))
-                                        <td class="text-center border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 flex">
-                                            <x-responsive-nav-link href="{{route('articles.edit', $article->id)}}">edit
-                                            </x-responsive-nav-link>
-                                        </td>
+                                        <x-responsive-nav-link href="{{route('articles.edit', $article->id)}}">edit
+                                        </x-responsive-nav-link>
                                     @endif
-                                </tr>
+                                    @if($article->isDraft())
+                                        <form action="{{route('article.publish', $article->id)}}" method="post">
+                                            @csrf
+                                            <x-secondary-button type="submit" class="mb-4">publish
+                                            </x-secondary-button>
+                                        </form>
+                                    @endif
+                                    @if(is_null($article->deleted_at))
+                                        <form action="{{route('articles.destroy', $article->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <x-danger-button class="mb-4">delete
+                                            </x-danger-button>
+                                        </form>
+                                    @else
+
+                                        <form action="{{route('articles.restore', $article->id)}}" method="post">
+                                            @csrf
+                                            <x-secondary-button type="submit" class="mb-4">restore
+                                            </x-secondary-button>
+                                        </form>
+                                    @endif
+
+                                </td>
+                            </tr>
                             </tbody>
                         </table>
                     @endauth
